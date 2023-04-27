f493559b1f8b7ba4956ab370b0fa3932835c796f
Zukhra <zuhra.galieva.2014@gmail.com>
2023-04-27 10:27:32 +0100

    Display current day

--- my-script.js
+++ my-script.js
@@ -1,7 +1,8 @@
 console.log("JavaScript is working!");
 
-const myImage2 = document.querySelector("#img-2");
 
+
+const myImage2 = document.querySelector("#img-2");
 myImage2.onclick = () => {
   const mySrc = myImage2.getAttribute("src");
   if (mySrc === "https://picsum.photos/id/1039/600/300") {
@@ -11,8 +12,8 @@ myImage2.onclick = () => {
   }
 };
 
-const myImage1 = document.querySelector("#img-1");
 
+const myImage1 = document.querySelector("#img-1");
 myImage1.onclick = () => {
   const mySrc = myImage1.getAttribute("src");
   if (mySrc === "https://picsum.photos/id/24/600/300") {
